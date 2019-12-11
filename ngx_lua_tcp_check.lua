c1559d65a40abb78105bea23e3486d7c9df7814a
frogl <freedom_hqh@foxmail.com>
2019-12-11 11:17:24 +0800

    nginx-lua read tcp stream

---
+++ ngx_lua_tcp_check.lua
@@ -0,0 +1,49 @@
+
+local function bin2hex(s)
+    s=string.gsub(s,"(.)",function (x) return string.format("%02X ",string.byte(x)) end)
+    return s
+end
+ 
+local h2b = {
+    ["0"] = 0,
+    ["1"] = 1,
+    ["2"] = 2,
+    ["3"] = 3,
+    ["4"] = 4,
+    ["5"] = 5,
+    ["6"] = 6,
+    ["7"] = 7,
+    ["8"] = 8,
+    ["9"] = 9,
+    ["A"] = 10,
+    ["B"] = 11,
+    ["C"] = 12,
+    ["D"] = 13,
+    ["E"] = 14,
+    ["F"] = 15
+}
+ 
+local function hex2bin( hexstr )
+    local s = string.gsub(hexstr, "(.)(.)%s", function ( h, l )
+         return string.char(h2b[h]*16+h2b[l])
+    end)
+    return s
+end
+
+local sock = assert(ngx.req.socket(true))
+local data = sock:peek(15)
+if not data then 
+    ngx.log(ngx.ERR,"payload null")
+    return
+end
+
+ngx.log(ngx.INFO,"data is:",data,"len:",string.len(data),"xxx:",bin2hex(data))
+
+
+t = string.byte(string.sub(data,8,8))
+s = string.sub(data,13,15)
+if  not ( s == "Log" and t==1) then 
+     ngx.log(ngx.ERR,"payload 13-15:",string.sub(data,13,15))
+     ngx.exit(404)
+end
+
